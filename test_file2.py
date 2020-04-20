f37d2a2c30068ebcbcc6d765c8f9445a9b31652b
nitinb29 <niitin@gmail.com>
2020-04-20 13:39:53 +0100

    C5 : test for git jenkins integration

--- test_file2.py
+++ test_file2.py
@@ -1,3 +1,7 @@
-for I in 1 .. 10 loop
-  Put_Line ("Hello world !!");
-end loop;
+count = 0
+while count < 10:
+    
+print(count)
+   
+("Hello world !!")
+count += 1  # This is the same as count = count + 1
